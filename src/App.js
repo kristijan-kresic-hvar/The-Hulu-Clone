bdc5aff97fd2694a682db541dd5bd43226cebcf5
kristijan-kresic-hvar <kristijan-kresic.dev@gmail.com>
2020-09-13 02:34:19 +0200

    Added Header UI

---
+++ src/App.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import './App.css';
+
+import Header from './Header';
+
+function App() {
+  return (
+    <div className="app">
+
+      <Header />
+
+      {/* Nav */}
+
+      {/* Results */}
+
+    </div>
+  );
+}
+
+export default App;
