955e09f0edb97d4937df228634f446b859694ebb
kristijan-kresic-hvar <kristijan-kresic.dev@gmail.com>
2020-09-17 01:43:57 +0200

    Added navbar UI

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import './App.css';
 
 import Header from './Header';
+import Nav from './Nav';
 
 function App() {
   return (
@@ -9,7 +10,7 @@ function App() {
 
       <Header />
 
-      {/* Nav */}
+      <Nav />
 
       {/* Results */}
 
